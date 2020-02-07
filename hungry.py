d4868a177c673876a4980f987110c34101931b94
soumya76 <pbsoumya7@gmail.com>
2020-02-07 13:43:40 +0530

    water

--- hungry.py
+++ hungry.py
@@ -7,3 +7,13 @@ print("eat burger")
 else:
 print("do your homework")
 print (“I am hungry”)
+hungry=input("are you hungry")
+if hungry=="yes":
+print("eat samosa")
+print("eat pizza")
+print("eat burger")
+
+else:
+print("do your homework")
+print (“I am hungry”)
+
